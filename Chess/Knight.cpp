3e59487b41d1c68c1b29b7cf2228dc70976b7f80
IconsumeBees <wrhnngr@gmail.com>
2024-12-25 20:10:25 +0200

    finished all the movement of the classes

--- Chess/Knight.cpp
+++ Chess/Knight.cpp
@@ -8,6 +8,6 @@ Knight::Knight(const char piece, const Loc& loc) : Piece(piece, loc)
 bool Knight::validMove(Piece* arr[8][8], const Loc& dst)
 {
 	Loc src = this->_loc;
-	if (dst.row - src.row + dst.col - src.col != 3 && dst.row - src.row != 0 && dst.row - src.row != 0) { return false; }
+	if (dst.row - src.row + dst.col - src.col != 3 && dst.row - src.row == 0 || dst.col - src.col == 0) { return false; }
 	return true;
 }
